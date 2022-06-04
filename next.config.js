299c21bab323670bab1700130f57745cd2429c39
cbm <christian.boffill@gmail.com>
2022-06-04 18:45:19 -0400

    Some configurations for static testing of the code and other necessary files were added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['s3.amazonaws.com', 'flic.kr'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
