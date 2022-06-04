39d39f35fd20a714f60c94cf2e8695ba84827867
cbm <christian.boffill@gmail.com>
2022-06-04 19:42:07 -0400

    Th Friend page was updated

--- src/pages/friend/[id].tsx
+++ src/pages/friend/[id].tsx
@@ -1,22 +1,26 @@
-import { NextPage } from 'next'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
+import { NextPage } from 'next';
+import Head from 'next/head';
+import { useAppSelector } from '@redux/hooks';
+import Details from '@layouts/Details';
+import GalleryModal from '@layouts/GalleryModal';
+
+import styles from '@styles/Friend.module.css';
 
 const Friend: NextPage = () => {
-  const router = useRouter()
+  const isOpen = useAppSelector((state) => state.modal.isOpen);
   return (
-    <div>
+    <div className={`${styles.friend} ${isOpen ? styles.friend_viewport : ''}`}>
       <Head>
         <title>Friend&apos;s Details</title>
         <meta name="description" content="Friend's Details" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>
-        Friend&apos;s ID
-        {router.query.id}
-      </p>
-    </div>
-  )
-}
 
-export default Friend
+      <Details />
+
+      {isOpen ? <GalleryModal /> : null}
+    </div>
+  );
+};
+
+export default Friend;
