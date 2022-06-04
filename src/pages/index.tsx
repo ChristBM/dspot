e6057cd104aec190044caacd964877e6dd8fc6e0
cbm <christian.boffill@gmail.com>
2022-06-04 19:19:23 -0400

    Home page was updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
-import { NextPage } from 'next'
-import Head from 'next/head'
+import { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '@styles/Home.module.css';
+import List from '@layouts/List';
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>List of Friends</title>
-        <meta name="description" content="List of Friends" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  )
-}
+const Home: NextPage = () => (
+  <div className={styles.home}>
+    <Head>
+      <title>List of Friends</title>
+      <meta name="description" content="List of Friends" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-export default Home
+    <List />
+  </div>
+);
+
+export default Home;
